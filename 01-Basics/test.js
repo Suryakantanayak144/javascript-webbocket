fa58cfa5b795b22f5a0e30f2ef69b6f74cf5df8e
Suryakantanayak144 <152247522+Suryakantanayak144@users.noreply.github.com>
2023-11-29 04:22:15 +0000

    file added

---
+++ 01-Basics/test.js
@@ -0,0 +1,6 @@
+console.log("namaste dunia");
+/*
+1. Javascrpt is a high level scripting language used on developing web and mobile application efficienowntly.
+We ise javascript in both client side and server side.
+it was developed in 1997.
+Now these days we used the Es6 version of javascript .It was started on 2015 and it is the latestversion.
