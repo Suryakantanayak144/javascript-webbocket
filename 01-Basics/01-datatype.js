86139ac55b35638a7ae8401a33a83b5e2ec0b50f
Suryakantanayak144 <152247522+Suryakantanayak144@users.noreply.github.com>
2023-11-30 04:44:41 +0000

    file added

---
+++ 01-Basics/01-datatype.js
@@ -0,0 +1,9 @@
+let number=34;
+let name ="suryakanta";
+let isPresent = true;
+let id = Symbol('123');
+let anotherId=Symbol('123');
+
+console.log(number);
+console.log(name);
+console.log(isPresent);
--- 01-Basics/test.js
+++ 01-Basics/test.js
@@ -4,3 +4,4 @@ console.log("namaste dunia");
 We ise javascript in both client side and server side.
 it was developed in 1997.
 Now these days we used the Es6 version of javascript .It was started on 2015 and it is the latestversion.
+*/
